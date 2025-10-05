69fd9488d2dc3790fbfd846c20910f248b3775cf
Dave Byrne <calamity.daveyjones@gmail.com>
2025-10-05 14:53:24 +0100

    loaded and printed json file

--- assignments/assignment02/assignment02-bankholidays.py
+++ assignments/assignment02/assignment02-bankholidays.py
@@ -1,3 +1,15 @@
 # write a program that prints out the dates of all the bank holidays in Northern Ireland
 # Author : Dave Byrne :)
+# References:
+# https://www.w3schools.com/python/python_json.asp
+# https://www.digitalocean.com/community/tutorials/python-pretty-print-json
 
+
+
+import json
+
+# load the JSON data from the file
+with open('assignments/assignment02/bank_holidays.json') as f:
+    data = json.load(f)
+
+print(json.dumps(data, indent=1))
